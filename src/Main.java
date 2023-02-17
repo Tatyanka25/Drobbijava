1b7376cb14e9ef5cd7ec795fc74cbe8b25fb0db1
Tatyanka25 <lapshinatatyana25@mail.ru>
2023-02-17 10:25:21 +0300

    create solution

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var fraction1 = new Drobbi(3, 5);
+        var fraction2= new Drobbi(4,7);
+        var result = fraction1.Addition(fraction2);
+        var result1 = fraction1.Multiplication(fraction2);
+        var result2 = fraction1.Deduction(fraction2);
+        var result3 = fraction1.Splitting(fraction2);
+        System.out.println(result);
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
     }
 }
